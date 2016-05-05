c1503afb3f9abfabb2ca2ab2c0ae00012556046a
mpnguyen <minhphu.it@gmail.com>
2016-05-05 22:20:25 +0700

    Update edit data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,4 +7,32 @@ linkedInApp.controller('LinkedInCtrl', function ($scope, $http) {
     $http.get('profile.json').success(function(data) {
         $scope.profile = data;
     });
+    
+    $scope.deleteSkill = function(skill) {
+        var index = $scope.profile.skills.indexOf(skill);
+        $scope.profile.skills.splice(index,1);
+    };
+    
+    $scope.addNewSkill = function() {
+        $scope.profile.skills.push(
+            {
+                "score": "00",
+                "skillname": "new skill.."
+            }
+        );
+    };
+    
+    $scope.deleteOtherSkill = function(skill) {
+        var index = $scope.profile.others.indexOf(skill);
+        $scope.profile.others.splice(index,1);
+    };
+    
+    $scope.addNewOtherSkill = function() {
+        $scope.profile.others.push(
+            {
+                "score": "00",
+                "skillname": "new skill.."
+            }
+        );
+    };
 });
